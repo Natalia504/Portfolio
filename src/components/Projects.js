562d30db2628ce3a0e6b264d5d77a13df4155571
MountainGirl504 <natalia_calt@yahoo.com>
2017-12-17 17:59:52 -0700

    text added

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -12,7 +12,7 @@ export default class Projects extends Component {
         <div className='wrapper-main '>
           <div className='container-box-projects'>
             <h2 className='animated fadeIn'>My Work</h2>
-            <p className='animated fadeIn'>Phasellus enim sapien, blandit <a href='https://s3.us-east-2.amazonaws.com/random-docs/NATALIA+CALT.pdf' target='_blank' rel='noopener noreferrer'>Resume</a> elementum eu, condimentum eu elit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia luctus elit eget interdum.</p>
+            <p className='animated fadeIn'>Here you'll find my <a href='https://s3.us-east-2.amazonaws.com/resume-natalia/NATALIA+CALT.pdf' target='_blank' rel='noopener noreferrer'>Resume</a>. I always work on new projects to learn new technologies and tricks. Check out my recent projects below. I never stop creating!</p>
             <div className='project-container animated fadeIn'>
               <Link to='/projectDetails'><div className='box a'>
               </div></Link>
