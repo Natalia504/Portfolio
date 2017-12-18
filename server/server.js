82fbd1e4bd5bae63db9ad61c5333513e58e7747f
MountainGirl504 <natalia_calt@yahoo.com>
2017-12-17 18:10:53 -0700

    done

--- server/server.js
+++ server/server.js
@@ -10,4 +10,10 @@ app.use(express.static(`${__dirname}/../build`));
 
 
 
+
+
+const path = require('path')
+app.get('*', (req, res)=>{
+  res.sendFile(path.join(__dirname, '../build/index.html'));
+})
 app.listen(port, () => (console.log(`Listening on port ${port}`)));
