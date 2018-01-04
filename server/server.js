8a102fbda6517f75a0cd7942e43146852d75d9cf
MountainGirl504 <natalia_calt@yahoo.com>
2018-01-03 19:22:35 -0700

    server is back

---
+++ server/server.js
@@ -0,0 +1,19 @@
+
+const express = require ('express'),
+    bodyParser = require ('body-parser'),
+    app = express(),
+    port = 5050
+
+app.use(bodyParser.json());
+app.use(express.static(`${__dirname}/../build`));
+
+
+
+
+
+
+const path = require('path')
+app.get('*', (req, res)=>{
+  res.sendFile(path.join(__dirname, '../build/index.html'));
+})
+app.listen(port, () => (console.log(`Listening on port ${port}`)));
