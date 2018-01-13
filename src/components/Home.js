56df72133a0c46fc4f61fbb48a3868b3100d3ff9
MountainGirl504 <natalia_calt@yahoo.com>
2018-01-13 09:52:54 -0700

    update

--- src/components/Home.js
+++ src/components/Home.js
@@ -15,7 +15,7 @@ export default class Home extends Component {
               <div className='content animated slideInDown'>
                 <div className='name'><h1 className='animated slideInDown'>Natalia Calt</h1></div>
                 <div className='title'><h3 className='animated slideInLeft'>Full Stack Web Developer</h3></div>
-                <div className='description'><blockquote className='animated slideInRight'>"And, when you want something, all the universe conspires in helping you to achieve it." Paulo Coelho, The Alchemist</blockquote></div>
+                <div className='description'><blockquote className='animated slideInRight'>And, when you want something, all the universe conspires in helping you to achieve it.<cite>&mdash;Paulo Coelho, The Alchemist</cite></blockquote></div>
               </div>
             </div>
             <div className='container-right animated fadeIn'></div>
