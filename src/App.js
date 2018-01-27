c3f9b0b1405df371f8803dfb85498b899d8ebb35
MountainGirl504 <natalia_calt@yahoo.com>
2018-01-27 00:35:49 -0700

    removed project details

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import React, { Component } from 'react';
 import Home from './components/Home';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
-import ProjectDetails from './components/ProjectDetails';
-import ProjectDetails2 from './components/ProjectDetails2';
-import ProjectDetails3 from './components/ProjectDetails3';
+// import ProjectDetails from './components/ProjectDetails';
+// import ProjectDetails2 from './components/ProjectDetails2';
+// import ProjectDetails3 from './components/ProjectDetails3';
 import { BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
 
@@ -18,9 +18,9 @@ class App extends Component {
           <Route exact path='/' component={Home} />
           <Route path='/myWork' component={Projects} />
           <Route path='/contact' component={Contact} />
-          <Route path='/projectDetails' component={ProjectDetails} />
+          {/* <Route path='/projectDetails' component={ProjectDetails} />
           <Route path='/projectDetails2' component={ProjectDetails2} />
-          <Route path='/projectDetails3' component={ProjectDetails3} />
+          <Route path='/projectDetails3' component={ProjectDetails3} /> */}
         </div>
       </BrowserRouter>
     );
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -14,17 +14,18 @@ export default class Projects extends Component {
             <h2 className='animated fadeIn'>My Work</h2>
             <p className='animated fadeIn'>This Portfolio is built from ground up and I'm planning on adding more projects to it. I never stop creating and always work on new projects to learn new tricks and technologies. Check out my recent projects below and <a href='https://s3.us-east-2.amazonaws.com/resume-natalia/NATALIA+CALT.pdf' target='_blank' rel='noopener noreferrer'>resume here.</a></p>
             <div className='project-container animated fadeIn'>
-              <Link to='/projectDetails'>
-                <div className='box a'><h3>Ecommerce Site</h3>
-                  <h5><br />React / Redux<br />Node / Express<br />PostreSQL<br />Stripe API<br />CSS3</h5>
+              
+                <div className='box a'><h3>E-commerce project</h3>
+                  <h5><a href='http://165.227.30.69:5050/' target='_blank' rel='noopener noreferrer'>Site Link</a><br/><a href="https://youtu.be/0zlThN-dr90" target='_blank' rel='noopener noreferrer'>Video Tour</a>
+                    <br />React / Redux<br />Node / Express<br />PostreSQL<br />Stripe API<br />CSS3</h5>
                 </div>
-              </Link>
-              <Link to='/projectDetails2'><div className='box b'><h3>Flashcard App</h3>
-                <h5><br />React / Redux<br />Node / Express<br />PostgeSQL<br />Auth0<br />CSS / SASS</h5>
+              
+              <div className='box b'><h3>Flashcard App</h3>
+                <h5><a href="https://youtu.be/342xlIa2_vc" target='_blank' rel='noopener noreferrer'>Video Tour</a><br />React / Redux<br />Node / Express<br />PostgeSQL<br />Auth0<br />CSS / SASS</h5>
               </div>
-              </Link>
-              <Link to='/projectDetails3'><div className='box c'><h3>Omnifood Webpage</h3><h5><br/>HTML5<br/>CSS3 <br/>jQuery</h5></div>
-              </Link>
+              
+              <div className='box c'><h3>Omnifood Webpage</h3><h5><a href='https://nataliacalt-omnifood.firebaseapp.com/' target='_blank' rel='noopener noreferrer'>Site Link</a><br/>HTML5<br/>CSS3 <br/>jQuery</h5></div>
+              
               <div className='box d'><h3>Work in progress...</h3>
               <h5>I'm currently building a new project in React <br/>but I'm not ready to share it yet.</h5></div>
             </div>
