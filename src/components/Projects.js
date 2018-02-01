c487860847014ad2255f2e7264b9cef83f149ed2
MountainGirl504 <natalia_calt@yahoo.com>
2018-02-01 12:25:49 -0700

    updates

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -9,25 +9,50 @@ export default class Projects extends Component {
     return (
       <div className='main-container'>
         <Navigation />
-        <div className='wrapper-main'>
+        <div className='wrapper-main-projects'>
           <div className='container-box-projects'>
             <h2 className='animated fadeIn'>My Work</h2>
             <p className='animated fadeIn'>This Portfolio is built from ground up and I'm planning on adding more projects to it. I never stop creating and always work on new projects to learn new tricks and technologies. Check out my recent projects below and <a href='https://s3.us-east-2.amazonaws.com/resume-natalia/NATALIA+CALT.pdf' target='_blank' rel='noopener noreferrer'>resume here.</a></p>
             <div className='project-container animated fadeIn'>
-              
-                <div className='box a'><h3>E-commerce project</h3>
-                  <h5><a href='http://165.227.30.69:5050/' target='_blank' rel='noopener noreferrer'>Site Link</a><br/><a href="https://youtu.be/0zlThN-dr90" target='_blank' rel='noopener noreferrer'>Video Tour</a>
-                    <br />React / Redux<br />Node / Express<br />PostreSQL<br />Stripe API<br />CSS3</h5>
-                </div>
-              
-              <div className='box b'><h3>Flashcard App</h3>
-                <h5><a href="https://youtu.be/342xlIa2_vc" target='_blank' rel='noopener noreferrer'>Video Tour</a><br />React / Redux<br />Node / Express<br />PostgeSQL<br />Auth0<br />CSS / SASS</h5>
+
+              <div className='box a'><h3>E-commerce project</h3><br/>
+                <h5>
+                  <a href='http://165.227.30.69:5050/' target='_blank' rel='noopener noreferrer'>Site Link</a><br />
+                  <a href="https://youtu.be/0zlThN-dr90" target='_blank' rel='noopener noreferrer'>Video Tour</a><br/>
+                  <h4>Technologies:</h4>
+                  React / Redux<br />
+                  Node / Express<br />
+                  PostreSQL<br />
+                  Stripe API<br />
+                  CSS3
+                </h5>
               </div>
-              
-              <div className='box c'><h3>Omnifood Webpage</h3><h5><a href='https://nataliacalt-omnifood.firebaseapp.com/' target='_blank' rel='noopener noreferrer'>Site Link</a><br/>HTML5<br/>CSS3 <br/>jQuery</h5></div>
-              
-              <div className='box d'><h3>Work in progress...</h3>
-              <h5>I'm currently building a new project in React <br/>but I'm not ready to share it yet.</h5></div>
+
+              <div className='box b'><h3>Flashcard App</h3><br/>
+                <h5>
+                  <a href="https://youtu.be/342xlIa2_vc" target='_blank' rel='noopener noreferrer'>Video Tour</a><br/>
+                  <h4>Technologies:</h4>
+                  React / Redux<br />
+                  Node / Express<br />
+                  PostgeSQL<br />
+                  Auth0<br />
+                  CSS / SASS
+                </h5>
+              </div>
+
+              <div className='box c'><h3>Omnifood Webpage</h3><br/>
+              <h5>
+                <a href='https://nataliacalt-omnifood.firebaseapp.com/' target='_blank' rel='noopener noreferrer'>Site Link</a><br/>
+                <a href="#" target='_blank' rel='noopener noreferrer'>Video Tour</a><br/>
+                <h4>Technologies:</h4>
+                HTML5<br/>
+                CSS3 <br/>
+                jQuery
+              </h5>
+              </div>
+
+              <div className='box d'><h3>Work in progress...</h3><br/><br/>
+                <h5>I'm currently building a new project in React <br />but it's not ready to be shared yet.</h5></div>
             </div>
           </div>
         </div>
